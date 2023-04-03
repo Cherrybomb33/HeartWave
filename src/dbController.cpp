54dee2f4bbca19af64a0efea4a4b3ab0b52d23ab
Cherrybomb33 <bluenews33@gmail.com>
2023-04-03 16:44:00 -0400

    update dbController.cpp

--- src/dbController.cpp
+++ src/dbController.cpp
@@ -1,3 +1,18 @@
 #include "dbController.h"
 
 const Qstring DBController::DATABASE_PATH = "/database/heartwave.db";
+
+DBController::DBController(){
+    heartwaveDB = = QSqlDatabase::addDatabase("QSQLITE");
+    heartwaveDB.setDatabaseName("hearwave.db");
+    
+    if (!heartwaveDB.open()) {
+        throw "Error: Database could not be opened";
+    }
+
+    if (!DBInit()) {
+        throw "Error: Database could not be initialized";
+    }
+
+
+}
