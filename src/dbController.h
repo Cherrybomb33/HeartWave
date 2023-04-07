325470f5cb703cebfbe48ce29c6356e27d3fd6ed
Cherrybomb33 <91170120+Cherrybomb33@users.noreply.github.com>
2023-04-06 21:00:27 -0400

    Update dbController.h

--- src/dbController.h
+++ src/dbController.h
@@ -9,6 +9,7 @@
 #include <QDebug>
 #include <QVariant>
 #include <QVector>
+#include <QVPointF>
 #include <QList>
 #include <QApplication>
 
@@ -20,21 +21,20 @@ public:
 
     const QString DATE_FORMAT = "yyyy-MM-dd hh:mm:ss";   //time format in database
     static const QString DATABASE_PATH;    //path of the database
+    
     QStringList getHistory();    //get a list of QString
     bool getRecord(const QDateTime& time, Record** record);   //get a Record object
-
+    
+    //add a session record to the database
     bool addRecord(const QDateTime& time, const int challengeLevel, const int length, const double lowPercentage, const double medPercentage, const double highPercentage, const double averageCoherence, const double achievementScore, const QVector<QPointF>& hrvGraph);
-    bool deleteRecord(const QDateTime& time);
-    //void resetDevice();    //  if isReset == true, call reset() and set isReset to false;
+    bool deleteRecord(const QDateTime& time);    //delete a record from the database
+    bool reset();   //wipe all records and restore the database to the initial condition
 
 private:
-    QSqlDatabase heartwaveDB;    //database object
-    //Setting setting;
-    //bool isReset;  //initialize to false      
+    QSqlDatabase heartwaveDB;    //database object     
 
-    bool dbInit();    //initializes the database
+    bool dbInit();    //initialize the database
     bool isValidRecord(const QDateTime& time, const int challengeLevel, const int length, const double lowPercentage, const double medPercentage, const double highPercentage, const double averageCoherence, const double achievementScore);
-    //void reset();   //wipe all data and restore the device to the initial install condition
 
 };
 
